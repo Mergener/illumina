bd02aa7ec5eac3080c6a2cea49cde49024fc925f
Thomas <mergener.thomas@gmail.com>
2024-04-11 12:27:01 -0300

    WIP see

---
+++ illumina/boardutils.cpp
@@ -0,0 +1,11 @@
+#include "boardutils.h"
+
+namespace illumina {
+
+bool has_good_see(const Board& board,
+                  Square source,
+                  Square dest,
+                  int threshold) {
+}
+
+} // illumina
