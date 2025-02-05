00fea4f074ed8f82bd507ab6d64c717f1522cdc3
Thomas <mergener.thomas@gmail.com>
2025-02-05 02:10:51 -0300

    -Wextra cleanup.

--- illumina/search.cpp
+++ illumina/search.cpp
@@ -465,7 +465,7 @@ void SearchWorker::aspiration_windows() {
     // Prepare the search stack.
     constexpr size_t STACK_SIZE = MAX_DEPTH + 64;
     SearchNode search_stack[STACK_SIZE];
-    for (Depth ply = 0; ply < STACK_SIZE; ++ply) {
+    for (Depth ply = 0; ply < Depth(STACK_SIZE); ++ply) {
         SearchNode& node = search_stack[ply];
         node.ply = ply;
     }
